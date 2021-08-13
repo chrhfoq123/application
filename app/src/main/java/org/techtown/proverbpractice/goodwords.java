5e9bcc70a7e83ea44ba513b0789951457445b0ed
박태광 <ptk725739@gmail.com>
2021-08-13 23:21:29 +0900

    21-08-13 살려줘

---
+++ app/src/main/java/org/techtown/proverbpractice/goodwords.java
@@ -0,0 +1,24 @@
+package org.techtown.proverbpractice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class goodwords extends Fragment {
+
+
+    public goodwords() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_goodwords, container, false);
+    }
+}
