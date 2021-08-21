eba021755e2875ddcc79d0ef8666e31d185b36d8
박태광 <ptk725739@gmail.com>
2021-08-21 21:43:49 +0900

    21-08-21 명언 새로고침 기능 커밋

--- app/src/main/java/org/techtown/proverbpractice/GoodWordFragment.java
+++ app/src/main/java/org/techtown/proverbpractice/GoodWordFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,6 +29,28 @@ public class GoodWordFragment extends Fragment {
 
         content = view.findViewById(R.id.content);
         content.setText(word.getWord().getContent());
+
+        Button reset = view.findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView title, comment, content;
+
+                Goodword word = new Goodword();
+
+                title = view.findViewById(R.id.title);
+                title.setText(Goodword.title);
+
+                comment = view.findViewById(R.id.comment);
+                comment.setText(word.getWord().getComment());
+
+                content = view.findViewById(R.id.content);
+                content.setText(word.getWord().getContent());
+
+            }
+        });
+
 
 
         return view;
