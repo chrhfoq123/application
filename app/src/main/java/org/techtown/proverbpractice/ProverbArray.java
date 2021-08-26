2829684c541bf4c6093d0d15128ea0f416e550ae
박태광 <ptk725739@gmail.com>
2021-08-26 22:30:37 +0900

    21-08-26 list View 사용방법 숙지 커밋

--- app/src/main/java/org/techtown/proverbpractice/ProverbArray.java
+++ app/src/main/java/org/techtown/proverbpractice/ProverbArray.java
@@ -1,11 +1,16 @@
 package org.techtown.proverbpractice;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,27 +27,33 @@ public class ProverbArray extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.proverbarrlayout, container,false);
 
-        textView = view.findViewById(R.id.textView);
+        //ArrayList<Box> list = getArguments().getParcelableArrayList("list");
+        //여기부터
+        ListView listview = view.findViewById(R.id.listView);
 
-        String result = "짜잔 아무것도 없음";
-        if(getArguments() != null){
-            s = getArguments().getString("name");
-            ArrayList<Box> list = getArguments().getParcelableArrayList("list");
-            if(list.size() != 0){
-                result = "";
-            }
-            for(int i=0; i<list.size(); i++){
-                Box box = list.get(i);
-                result += "1. 분류 => "+ box.category +"";
-                result += "2. 내용 => "+ box.comment +"";
-                result += "3. 설명 => "+ box.content +"";
-                result += "\n";
+        //컨택스트 객채 생성 외울것
+        Context context = getActivity().getApplicationContext();
 
-            }
+        //db객채 생성 "db이름", "뭔지모름외워야함", null
+        SQLiteDatabase db = context.openOrCreateDatabase("ptk_db", Context.MODE_PRIVATE, null);
+
+        Cursor cursor = db.rawQuery("SELECT * FROM comment_list", null);
+
+        ArrayList<String> testarray = new ArrayList<String>();
+
+        while (cursor.moveToNext()) {
+            String category = cursor.getString(0);
+            String comment = cursor.getString(1);
+            String content = cursor.getString(2);
+            //Box obj = new Box("category","comment","content");
+            testarray.add(comment);
         }
-        textView.setText(result);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, testarray);
+        listview.setAdapter(adapter);
 
 
         return view;
     }
 }
+
